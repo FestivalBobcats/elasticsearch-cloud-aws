edead66b5bb24c70b71801e32c54432f4035154d
Ben Hundley <ben.hundley@gmail.com>
2014-11-09 13:18:44 -0600

    support for eu-central-1 region with endpoint derivation

--- src/main/java/org/elasticsearch/cloud/aws/AwsEc2Service.java
+++ src/main/java/org/elasticsearch/cloud/aws/AwsEc2Service.java
@@ -123,6 +123,8 @@ public class AwsEc2Service extends AbstractLifecycleComponent<AwsEc2Service> {
                 endpoint = "ec2.eu-west-1.amazonaws.com";
             } else if (region.equals("sa-east") || region.equals("sa-east-1")) {
                 endpoint = "ec2.sa-east-1.amazonaws.com";
+            } else if (region.equals("eu-central") || region.equals("eu-central-1")) {
+                endpoint = "ec2.eu-central-1.amazonaws.com";
             } else {
                 throw new ElasticsearchIllegalArgumentException("No automatic endpoint could be derived from region [" + region + "]");
             }
--- src/main/java/org/elasticsearch/cloud/aws/InternalAwsS3Service.java
+++ src/main/java/org/elasticsearch/cloud/aws/InternalAwsS3Service.java
@@ -175,6 +175,10 @@ public class InternalAwsS3Service extends AbstractLifecycleComponent<AwsS3Servic
             return "s3-sa-east-1.amazonaws.com";
         } else if ("sa-east-1".equals(region)) {
             return "s3-sa-east-1.amazonaws.com";
+        } else if ("eu-central".equals(region)) {
+            return "s3-eu-central-1.amazonaws.com";
+        } else if ("eu-central-1".equals(region)) {
+            return "s3-eu-central-1.amazonaws.com";
         } else {
             throw new ElasticsearchIllegalArgumentException("No automatic endpoint could be derived from region [" + region + "]");
         }
